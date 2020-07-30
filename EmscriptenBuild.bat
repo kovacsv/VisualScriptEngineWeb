d49f10d4b9e053a08da88d1c91f21fd40e1dc5b6
Viktor Kovacs <viktorkovacs@gmail.com>
2020-07-31 00:00:35 +0200

    Rename main.html to index.html.

--- EmscriptenBuild.bat
+++ EmscriptenBuild.bat
@@ -9,7 +9,7 @@ call %1\emsdk_env.bat
 call emmake mingw32-make
 popd
 
-copy Sources\WebApp\main.html %2\main.html >NUL
+copy Sources\WebApp\index.html %2\index.html >NUL
 copy EmBuild\Release\WebApp.js %2\WebApp.js >NUL
 copy EmBuild\Release\WebApp.wasm %2\WebApp.wasm >NUL
 copy EmBuild\Release\WebApp.data %2\WebApp.data >NUL
