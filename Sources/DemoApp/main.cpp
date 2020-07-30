55ed3ed5213d3a1a3f3fbedba6838e7d96509718
Viktor Kovacs <viktorkovacs@gmail.com>
2020-07-30 12:34:11 +0200

    Add html file for displaying the result.

--- Sources/DemoApp/main.cpp
+++ Sources/DemoApp/main.cpp
@@ -4,13 +4,42 @@
 #endif
 #include <SDL.h>
 
+class SDLContext
+{
+public:
+	SDLContext () :
+		renderer (nullptr)
+	{
+
+	}
+
+	SDL_Renderer*	renderer;
+};
+
+void MainLoop (void* arg)
+{
+	SDLContext* context = (SDLContext*) arg;
+	SDL_SetRenderDrawColor (context->renderer, 0, 255, 0, 255);
+	SDL_RenderClear (context->renderer);
+
+	SDL_RenderPresent (context->renderer);
+}
+
 int main (int, char**)
 {
+	SDLContext context;
+
 	SDL_Init (SDL_INIT_VIDEO);
 	SDL_Window *window;
 	SDL_Renderer *renderer;
 	SDL_CreateWindowAndRenderer (255, 255, 0, &window, &renderer);
 	
+	context.renderer = renderer;
+
+#ifdef EMSCRIPTEN
+	emscripten_set_main_loop_arg (MainLoop, &context, 0, true);
+#endif
+
 	SDL_DestroyRenderer (renderer);
 	SDL_DestroyWindow (window);
 	SDL_Quit ();
--- emscripten_build.bat
+++ emscripten_build.bat
@@ -3,12 +3,15 @@
 pushd %~dp0
 
 mkdir EmBuild
-pushd EmBuild
 
+pushd EmBuild
 call %1\emsdk_env.bat
 call emcmake cmake -G "Unix Makefiles" -DEMSCRIPTEN=1 -DCMAKE_MAKE_PROGRAM=mingw32-make ..
 call emmake mingw32-make
-
 popd
+
+copy Sources\DemoApp\main.html %2\main.html >NUL
+copy EmBuild\DemoApp.js %2\DemoApp.js >NUL
+copy EmBuild\DemoApp.wasm %2\DemoApp.wasm >NUL
 
 popd
