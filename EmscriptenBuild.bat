aa39f14c6d3530a7aedc63e45c43a6b20d3a4977
Viktor Kovacs <viktorkovacs@gmail.com>
2020-07-31 20:56:19 +0200

    Modify website source structure.

--- EmscriptenBuild.bat
+++ EmscriptenBuild.bat
@@ -10,11 +10,7 @@ call emcmake cmake -G "Unix Makefiles" -DEMSCRIPTEN=1 -DCMAKE_MAKE_PROGRAM=mingw
 call emmake mingw32-make
 popd
 
-copy Sources\WebSite\index.html %2\index.html > NUL
-copy Sources\WebSite\style.css %2\style.css > NUL
-copy Sources\WebSite\folder_opened.png %2\folder_opened.png > NUL
-copy Sources\WebSite\folder_closed.png %2\folder_closed.png > NUL
-copy Sources\WebSite\plus.png %2\plus.png > NUL
+xcopy Sources\WebSite\*.* %2 /s /i /Y > NUL
 copy EmBuild\Release\WebApp.js %2\WebApp.js > NUL
 copy EmBuild\Release\WebApp.wasm %2\WebApp.wasm > NUL
 copy EmBuild\Release\WebApp.data %2\WebApp.data > NUL
