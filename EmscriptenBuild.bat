e9123360b385667ba1eb5806361826d9773795d5
Viktor Kovacs <viktorkovacs@gmail.com>
2020-08-12 19:27:40 +0200

    Modify the name of the build folder.

--- EmscriptenBuild.bat
+++ EmscriptenBuild.bat
@@ -1,9 +1,9 @@
 @echo %1
 
 pushd %~dp0
-mkdir EmBuild
+mkdir BuildEm
 
-pushd EmBuild
+pushd BuildEm
 call %1\emsdk_env.bat || goto :error
 call emcmake cmake -G "Unix Makefiles" -DEMSCRIPTEN=1 -DCMAKE_MAKE_PROGRAM=mingw32-make -DCMAKE_BUILD_TYPE=Release .. || goto :error
 call emmake mingw32-make || goto :error
@@ -11,9 +11,9 @@ popd
 echo Build Succeeded.
 
 xcopy Sources\WebSite\*.* %2 /s /i /Y > NUL
-copy EmBuild\Release\WebApp.js %2\WebApp.js > NUL
-copy EmBuild\Release\WebApp.wasm %2\WebApp.wasm > NUL
-copy EmBuild\Release\WebApp.data %2\WebApp.data > NUL
+copy BuildEm\Release\WebApp.js %2\WebApp.js > NUL
+copy BuildEm\Release\WebApp.wasm %2\WebApp.wasm > NUL
+copy BuildEm\Release\WebApp.data %2\WebApp.data > NUL
 
 popd
 exit /b 0
