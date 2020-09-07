118f5e75e99884c0650685c93a84c8acf5fb76c4
Viktor Kovacs <viktorkovacs@gmail.com>
2020-09-07 19:23:36 +0200

    Parameter settings design.

--- Sources/WebSite/editor/parametersettings.js
+++ Sources/WebSite/editor/parametersettings.js
@@ -154,8 +154,8 @@ ParameterSettings.prototype.GenerateButtons = function ()
 {
 	var popUpDivElem = this.popUpDiv.GetDiv ();
 	var buttonsDiv = $('<div>').addClass ('parameterbuttons').appendTo (popUpDivElem);
-	var okButton = $('<div>').addClass ('parameterbutton').html ('OK').appendTo (buttonsDiv);
-	var cancelButton = $('<div>').addClass ('parameterbutton').html ('Cancel').appendTo (buttonsDiv);
+	var okButton = $('<div>').addClass ('parameterbutton').addClass ('ok').html ('OK').appendTo (buttonsDiv);
+	var cancelButton = $('<div>').addClass ('parameterbutton').addClass ('cancel').html ('Cancel').appendTo (buttonsDiv);
 	
 	var myThis = this;
 	cancelButton.click (function () {
