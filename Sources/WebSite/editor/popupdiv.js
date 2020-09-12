0149da75a2818d738fbb3db36f7b5c48a0f24fae
Viktor Kovacs <viktorkovacs@gmail.com>
2020-09-12 08:39:57 +0200

    Do not allow context menu in popups.

--- Sources/WebSite/editor/popupdiv.js
+++ Sources/WebSite/editor/popupdiv.js
@@ -22,6 +22,10 @@ PopUpDiv.prototype.Open = function (positionX, positionY)
 		}
 	};
 	documentBody.bind ('mousedown', this.mouseDownHandler);
+
+	this.popUpDiv.contextmenu (function (ev) {
+		ev.preventDefault ();
+	});
 	
 	if (this.eventHandlers.onOpen) {
 		this.eventHandlers.onOpen ();
