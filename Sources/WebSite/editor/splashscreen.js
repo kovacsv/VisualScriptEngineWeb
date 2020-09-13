ea5d27768c34ef120dd656b032b70661ffa808cb
Viktor Kovacs <viktorkovacs@gmail.com>
2020-09-13 21:39:27 +0200

    Minor modification.

--- Sources/WebSite/editor/splashscreen.js
+++ Sources/WebSite/editor/splashscreen.js
@@ -13,14 +13,14 @@ SplashScreen.prototype.Show = function (mainDiv)
 	this.mainDiv.hide ();
 
 	var myThis = this;
-	this.timeout = window.setTimeout (function () {
+	this.timeout = setTimeout (function () {
 		var windowObj = $(window);
 		var bodyObj = $(document.body);
 		myThis.splashDiv = $('<div>').addClass ('splashscreen').appendTo (bodyObj);
 		$('<img>').attr ('src', myThis.imagePath).appendTo (myThis.splashDiv);
 		myThis.splashDiv.offset ({
 			left : windowObj.width () / 2 - myThis.splashDiv.outerWidth () / 2,
-			top : windowObj.height () / 4 - myThis.splashDiv.outerHeight () / 2
+			top : windowObj.height () / 3 - myThis.splashDiv.outerHeight () / 2
 		});
 	}, 500);
 	
@@ -28,7 +28,7 @@ SplashScreen.prototype.Show = function (mainDiv)
 
 SplashScreen.prototype.Hide = function ()
 {
-	window.clearTimeout (this.timeout);
+	clearTimeout (this.timeout);
 	if (this.splashDiv != null) {
 		this.splashDiv.remove ();
 	}
