9c72b6736735e67df1c0b4b4f75f875f699e3fbc
Viktor Kovacs <viktorkovacs@gmail.com>
2020-10-19 21:01:45 +0200

    Revert "Try to fix path on AppVeyor."
    This reverts commit e3844825b3f341efc221434acb1508e14be64853.

--- EmscriptenBuild.bat
+++ EmscriptenBuild.bat
@@ -1,15 +1,16 @@
 pushd %~dp0
 mkdir BuildEm
 
+SET SAVED_PATH=%PATH%
+
 pushd BuildEm
-SET ORIGINAL_PATH=%PATH%
 call %1\emsdk_env.bat || goto :error
-SET PATH=%PATH%;%ORIGINAL_PATH%
 call emcmake cmake -G "Unix Makefiles" -DEMSCRIPTEN=1 -DCMAKE_MAKE_PROGRAM=mingw32-make -DCMAKE_BUILD_TYPE=Release .. || goto :error
 call emmake mingw32-make || goto :error
 popd
 echo Build Succeeded.
 
+SET PATH=%SAVED_PATH%
 xcopy Sources\WebSite\*.* %2 /s /i /Y > NUL
 copy BuildEm\Release\WebApp.js %2\WebApp.js > NUL
 copy BuildEm\Release\WebApp.wasm %2\WebApp.wasm > NUL
@@ -19,6 +20,7 @@ popd
 exit /b 0
 
 :error
+SET PATH=%SAVED_PATH%
 echo Build Failed with Error %errorlevel%.
 popd
 popd
