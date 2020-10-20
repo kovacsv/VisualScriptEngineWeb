48307246c890df032e2bb58daade61d8ca74891c
Viktor Kovacs <viktorkovacs@gmail.com>
2020-10-20 06:51:20 +0200

    Set build configuration as a parameter for build script.

--- EmscriptenBuild.bat
+++ EmscriptenBuild.bat
@@ -3,7 +3,7 @@ mkdir BuildEm
 
 pushd BuildEm
 call %1\emsdk_env.bat || goto :error
-call emcmake cmake -G "Unix Makefiles" -DEMSCRIPTEN=1 -DCMAKE_MAKE_PROGRAM=mingw32-make -DCMAKE_BUILD_TYPE=Release .. || goto :error
+call emcmake cmake -G "Unix Makefiles" -DEMSCRIPTEN=1 -DCMAKE_MAKE_PROGRAM=mingw32-make -DCMAKE_BUILD_TYPE=%3 .. || goto :error
 call emmake mingw32-make || goto :error
 popd
 echo Build Succeeded.
